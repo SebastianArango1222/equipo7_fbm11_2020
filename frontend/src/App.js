fb7215a494f70a8cd3d53e9f7e1e901a18bf5ea1
SebastianArango1222 <xebaxlolgamer@gmail.com>
2020-09-23 11:47:05 -0500

    Se crea el proyecto frontend en react.

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>La voz de la elección del gobierno escolar</h1>
+    </div>
+  );
+}
+
+export default App;
