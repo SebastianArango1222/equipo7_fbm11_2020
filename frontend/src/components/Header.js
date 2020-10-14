6e628d74264ec57457057dd61e05f72d98e02989
Micpric <micperezrojas@gmail.com>
2020-10-13 22:59:46 -0500

    Se añadió zona de propuestas, zona de candidatos, el inicio, y el apartado para acerca de, aún falta agregar estilos

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,20 +4,14 @@ class Header extends Component {
     render() {
         return (
             <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-<<<<<<< HEAD
+
                 <h5 className="my-0 mr-md-auto font-weight-normal">Voz de la elección al gobierno escolar</h5>
                 <nav className="as">
                     <a className="p-2 text-dark" href="nose.js">Propuestas</a>
                     <a className="p-2 text-dark" href="#">Quiénes somos</a>
-                    <a className="p-2 text-dark" href="#">aja</a>
-=======
-                <h5 className="my-0 mr-md-auto font-weight-normal">La Voz De La Elección Del Gobierno Escolar</h5>
-                <nav className="my-2 my-md-0 mr-md-3">
-                    <a className="p-2 text-dark" href="#">Features</a>
-                    <a className="p-2 text-dark" href="#">Enterprise</a>
-                    <a className="p-2 text-dark" href="#">Support</a>
->>>>>>> 6994d9bd183b41a9075f29f8c4d7df660f29087a
-                    <a className="p-2 text-dark" href="#">Pricing</a>
+                    <a className="p-2 text-dark" href="#">Acerca de</a>
+
+              
                 </nav>
                 <a className="btn btn-outline-primary" href="#">Iniciar Sesión</a>
             </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Inicio from './pages/Inicio';
-<<<<<<< HEAD
+
 import Paginaprincipal from './pages/Paginaprincipal';
 import App from './routes/App';
-=======
-import Login from './components/Login'
-import Signin from './components/Signin';
->>>>>>> 6994d9bd183b41a9075f29f8c4d7df660f29087a
+
+
+
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,13 +16,11 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
+
    {/* <App/>  */}
     <Paginaprincipal />
-=======
-  <Login />
-  <Signin />
->>>>>>> 6994d9bd183b41a9075f29f8c4d7df660f29087a
+    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
