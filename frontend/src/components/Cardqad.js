2da9b58b5b82b6984fe4294d650d3301a4aec6c9
Micpric <micperezrojas@gmail.com>
2020-11-18 15:02:08 -0500

    Implementación de las otras recomendaciones

--- frontend/src/components/Cardqad.js
+++ frontend/src/components/Cardqad.js
@@ -5,75 +5,78 @@ import Arango from '../Images/arango.jpg'
 import Miguel from '../Images/Miguel.jpg'
 import Lemus from '../Images/Lemus.jpg'
 import Luisa from '../Images/Luisa.jpg'
-class Card extends React.Component{
-    render(){
-        return(
-            <div className="card-deck mb-3 text-center">
-    <div className="card mb-4 shadow-sm">
-      <div className="card-header">
-          <h4 className="my-0 font-weight-normal">Sebastian Arango Diaz</h4>
-      </div>
-      <div className="card-body">
-        
-        <ul className="list-unstyled mt-3 mb-4">
-         
-        <img className ="Silueta2"src= {Arango}/>
-          
-          
-        </ul>
-        <li className="p-2 text-dark" >Programador y diseñador</li>     
-         </div>
-    </div>
-    <div className="card mb-4 shadow-sm">
-      <div className="card-header">
-        <h4 className="my-0 font-weight-normal">Miguel Angel Pérez Rojas</h4>
-      </div>
-      <div className="card-body">
-        
-        <ul className="list-unstyled mt-3 mb-4">
-        <img className ="Silueta2"src= {Miguel}/>
-        </ul>
-        <li className="p-2 text-dark" >Programador y diseñador</li>   
-      </div>
-    </div>
-    <div className="card mb-4 shadow-sm">
-      <div className="card-header">
-        <h4 className="my-0 font-weight-normal">Juan Sebastián Suárez Lemus</h4>
-      </div>
-      <div className="card-body">
-       
-        <ul className="list-unstyled mt-3 mb-4">
-        <img className ="Silueta2"src= {Lemus}/>
-        
-        </ul>
-        
-        <li className="p-2 text-dark" >Programador y diseñador</li>   
-        
-        
-       
-      </div>
-      
-       <div className="card-header">
-        <h4 className="my-0 font-weight-normal">Luisa Fernanda Alvarez Diaz</h4>
-      </div>
-      <div className="card-body">
-       
-        <ul className="list-unstyled mt-3 mb-4">
-        <img className ="Silueta2"src= {Luisa}/>
-        
-        </ul>
-        
-        <li className="p-2 text-dark" >Programador y diseñador</li>   
+class Card extends React.Component {
+  render() {
+    return (
+      <main role="main" className="body">
+        <a className="linkec" href="Inicio"  ><img className="as" src={Atras} /></a>
 
-        
-        
-       
-      </div>
-      <a  className="linkec"href="Inicio"  ><img className ="as"src= {Atras}/></a>
+        <div className="card-deck mb-3 text-center">
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">Sebastian Arango Diaz</h4>
+            </div>
+            <div className="card-body">
 
-    </div>
-  </div>
-        )
-    }
+              <ul className="list-unstyled mt-3 mb-4">
+
+                <img className="Silueta2" src={Arango} />
+
+
+              </ul>
+              <li className="p-2 text-dark" >Programador y diseñador</li>
+            </div>
+          </div>
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">Miguel Angel Pérez Rojas</h4>
+            </div>
+            <div className="card-body">
+
+              <ul className="list-unstyled mt-3 mb-4">
+                <img className="Silueta2" src={Miguel} />
+              </ul>
+              <li className="p-2 text-dark" >Programador y diseñador</li>
+            </div>
+          </div>
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">Juan Sebastián Suárez Lemus</h4>
+            </div>
+            <div className="card-body">
+
+              <ul className="list-unstyled mt-3 mb-4">
+                <img className="Silueta2" src={Lemus} />
+
+              </ul>
+
+              <li className="p-2 text-dark" >Programador y diseñador</li>
+
+
+
+            </div>
+
+            <div className="card-header">
+              <h4 className="my-0 font-weight-normal">Luisa Fernanda Alvarez Diaz</h4>
+            </div>
+            <div className="card-body">
+
+              <ul className="list-unstyled mt-3 mb-4">
+                <img className="Silueta2" src={Luisa} />
+
+              </ul>
+
+              <li className="p-2 text-dark" >Programador y diseñador</li>
+
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </main>
+    )
+  }
 }
 export default Card;
--- frontend/src/components/Inicioad.js
+++ frontend/src/components/Inicioad.js
@@ -19,39 +19,18 @@ class Inicio extends React.Component{
         </div>
           <div className="card-header">
            
-            <h5 className="my-0 font-weight-normal">La Institución Educativa Félix de Bedout Moreno, desde el año 2005 ofrece a la comunidad del sector,
- la educación en media técnica en informática con énfasis en diseño y desarrollo de software </h5>
+          <h5 className="my-0 font-weight-normal">La Institución Educativa Félix de Bedout Moreno, desde el año 2005 ofrece a lacomunidad del sector, la educación en media técnica en informática con énfasis en diseñoy desarrollo de software en convenido con el Politécnico Jaime Isaza Cadavid,implementando la estrategia de proyectos para el desarrollo de las competencias porparte de los estudiantes. </h5>
           </div>
 
           <div className="card mb-4 shadow-sm">
             <div className="card-header">
-              <h5 className="my-0 font-weight-normal"> en convenido con el Politécnico Jaime Isaza Cadavid, implementando la estrategia de proyectos para el desarrollo de las competencias por parte de los estudiantes. </h5>
+              <h5 className="my-0 font-weight-normal"> Teniendo en cuenta esta experiencia para el año 2018 laInstitución Educativa le apuesta al desarrollo de la metodología por proyectos para todoslos estudiantes desde el grado de transición hasta el grado undécimo, teniendo comoreferencia este proceso; convencidos de que es el medio para desarrollar competenciasobligatorias, competencias del siglo XXI y competencias trasversales a cualquier área delconocimiento. </h5>
+
+
             </div>
-            <div className="card mb-4 shadow-sm">
-              <div className="card-header">
-                <h5 className="my-0 font-weight-normal">Teniendo en cuenta esta experiencia para el año 2018 laInstitución Educativa le apuesta al desarrollo de la metodología por proyectos para todos los estudiantes</h5>
-
-              </div>
-              <div className="card mb-4 shadow-sm">
-                <div className="card-header">
-                  <h5 className="my-0 font-weight-normal">desde el grado de transición hasta el grado undécimo, teniendo comoreferencia este proceso; convencidos de que es el medio para desarrollar competenciasobligatorias, competencias del siglo XXI y competencias trasversales a cualquier área delconocimiento.</h5>
-                </div>
-                
-              </div>
-            </div>
-
-
 
           </div>
-
-
-
-
-
-    
         </div>
-
-
       </main>
        )
     }
